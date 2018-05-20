0903490668943b3ede11da216df24426cfea30a3
NAKAGAWA Chubei <nakagawa.chubei@gmail.com>
2018-05-21 00:10:57 +0900

    add font settings

---
+++ sh/once.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# gnustep japanese font
+defaults write NSGlobalDomain NSFont Takaoゴシック
+defaults write NSGlobalDomain NSUserFont Takaoゴシック
+defaults write NSGlobalDomain NSBoldFont Takaoゴシック
+
