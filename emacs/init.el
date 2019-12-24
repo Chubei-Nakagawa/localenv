63fa6195d36f9bfa7ce3f1268d071fd252066f69
NAKAGAWA Chubei <nakagawa.chubei@gmail.com>
2019-12-25 00:35:51 +0900

    add emacs setting

---
+++ emacs/init.el
@@ -0,0 +1,15 @@
+(setq inhibit-startup-message t)
+
+(setq make-backup-files nil)
+(setq delete-auto-save-files t)
+
+(setq-default tab0width 4 indent-tabs-mode nil)
+
+(menu-bar-mode -1)
+
+(tool-bar-mode -1)
+
+(require 'dired-x)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
