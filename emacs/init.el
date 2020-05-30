a23272275f45e83b8962b3c59751f75a0c31d905
NAKAGAWA.Chubei <nakagawa.chubei@gmail.com>
2020-05-30 12:07:38 +0900

    editor settings

--- emacs/init.el
+++ emacs/init.el
@@ -3,7 +3,8 @@
 (setq make-backup-files nil)
 (setq delete-auto-save-files t)
 
-(setq-default tab-width 4 indent-tabs-mode nil)
+(setq-default tab-width 4
+              indent-tabs-mode nil)
 
 (menu-bar-mode -1)
 
@@ -13,3 +14,10 @@
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
+(defun add-cpp-mode-func ()
+  (c-set-style "strowstrup")
+  (setq c-basic-offset 4)
+  )
+
+(add-hook 'c-mode-hook 'add-cpp-mode-func)
+(add-hook 'c++-mode-hook 'add-cpp-mode-func)
